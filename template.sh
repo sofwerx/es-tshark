e2bc2355539c32008b197a7a97bd01f7969317de
Ian Blenke <ian@blenke.com>
2018-02-28 17:07:22 -0500

    Trying to sort out why the template settings are not applying to new indicies

--- template.sh
+++ template.sh
@@ -5,17 +5,39 @@ CREDS=${CREDS:--u ${ES_USER}:${ES_PASSWORD}}
 
 if [ -n "$DELETE_EXISTING_TEMPLATE" ]; then
   curl -sXDELETE ${CREDS} 'http://'$ES_HOST'/_template/pcap-template'
+  echo ""
 fi
 
 if [ -n "$DELETE_ALL_INDEXES" ]; then
   curl -sXGET ${CREDS} 'http://'$ES_HOST'/_cat/indices?v' | grep pcap | while read line; do
     index="$(echo $line | awk '{print $3}')"
     curl -sXDELETE ${CREDS} 'http://'$ES_HOST'/'$index
+    echo ""
   done
+  echo ""
 fi
 
-exec curl -H "Content-Type: application/json" -sXPUT ${CREDS} 'http://'$ES_HOST'/_template/pcap-template' --data-binary '{
-    "template": "pcap-*",
+curl -H "Content-Type: application/json" -sXPUT ${CREDS} 'http://'$ES_HOST'/_template/pcap-template' --data-binary '{
+    "index_patterns": [ "pcap-*" ],
     "index.mapping.total_fields.limit" : "5000",
     "index.highlight.max_analyzed_offset": 100000
 }'
+
+echo ""
+
+curl -sXGET ${CREDS} 'http://'$ES_HOST'/_cat/indices?v' | grep pcap | while read line; do
+  index="$(echo $line | awk '{print $3}')"
+    curl -H "Content-Type: application/json" -sXPUT ${CREDS} 'http://'$ES_HOST'/'$index'/_settings' --data-binary '{
+    "index" : {
+      "mapping.total_fields.limit" : "5000",
+      "highlight.max_analyzed_offset": 100000
+     }
+}'
+    curl -H "Content-Type: application/json" -sXGET ${CREDS} 'http://'$ES_HOST'/'$index'/_settings'
+  done
+
+echo ""
+
+curl -H "Content-Type: application/json" -sXGET ${CREDS} 'http://'$ES_HOST'/_template/pcap-template'
+
+echo ""
